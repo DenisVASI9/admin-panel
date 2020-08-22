9aeb9ca9c271f6866e8c2e6e00c36adbb7aec2a6
Denis <denisvasilenkoise@outlook.com>
2020-08-22 17:07:48 +0500

    add mobile users

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -10,70 +10,82 @@ import {
     TableItem,
     TableItemText
 } from '../../components/table'
+import {useWindowSize} from "../../core/hooks/useWindowSize";
+
+const DesktopUsers = (props) => {
+    return <Table>
+        <TableHeader>
+            <Row>
+                <Col xs={6}><TableHeaderItem>ID</TableHeaderItem></Col>
+                <Col xs={6}><TableHeaderItem>Имя</TableHeaderItem></Col>
+                <Col xs={6}><TableHeaderItem>Телефон</TableHeaderItem></Col>
+                <Col xs={6}><TableHeaderItem>Действия</TableHeaderItem></Col>
+            </Row>
+        </TableHeader>
+        <TableItem>
+            <Row align="center">
+                <Col xs={6}>
+                    <TableItemText>bvgv-2342-23fv-svvf</TableItemText>
+                </Col>
+                <Col xs={6}>
+                    <TableItemText>Аркаша Петухов</TableItemText>
+                </Col>
+                <Col xs={6}>
+                    <TableItemText>+7 (324) - 23 - 234</TableItemText>
+                </Col>
+                <Col xs={6}>
+                    <Button type="primary">Открыть</Button>
+                </Col>
+            </Row>
+        </TableItem>
+        <TableItem>
+            <Row align="center">
+                <Col xs={6}>
+                    <TableItemText>bvgv-2342-23fv-svvf</TableItemText>
+                </Col>
+                <Col xs={6}>
+                    <TableItemText>Аркаша Петухов</TableItemText>
+                </Col>
+                <Col xs={6}>
+                    <TableItemText>+7 (324) - 23 - 234</TableItemText>
+                </Col>
+                <Col xs={6}>
+                    <Button type="primary">Открыть</Button>
+                </Col>
+            </Row>
+        </TableItem>
+        <TableItem>
+            <Row align="center">
+                <Col xs={6}>
+                    <TableItemText>bvgv-2342-23fv-svvf</TableItemText>
+                </Col>
+                <Col xs={6}>
+                    <TableItemText>Аркаша Петухов</TableItemText>
+                </Col>
+                <Col xs={6}>
+                    <TableItemText>+7 (324) - 23 - 234</TableItemText>
+                </Col>
+                <Col xs={6}>
+                    <Button type="primary">Открыть</Button>
+                </Col>
+            </Row>
+        </TableItem>
+    </Table>
+}
+
+const MobileUsers = (props) => {
+    return <div>Мобильные юзеры</div>
+}
 
 const UsersPage = () => {
+
+    const {width} = useWindowSize()
+
     return <MainLayout>
         <PageWrapper>
             <Page>
                 <PageTitle>Список пользователей</PageTitle>
-                <Table>
-                    <TableHeader>
-                        <Row>
-                            <Col xs={6}><TableHeaderItem>ID</TableHeaderItem></Col>
-                            <Col xs={6}><TableHeaderItem>Имя</TableHeaderItem></Col>
-                            <Col xs={6}><TableHeaderItem>Телефон</TableHeaderItem></Col>
-                            <Col xs={6}><TableHeaderItem>Действия</TableHeaderItem></Col>
-                        </Row>
-                    </TableHeader>
-                    <TableItem>
-                        <Row align="center">
-                            <Col xs={6}>
-                                <TableItemText>bvgv-2342-23fv-svvf</TableItemText>
-                            </Col>
-                            <Col xs={6}>
-                                <TableItemText>Аркаша Петухов</TableItemText>
-                            </Col>
-                            <Col xs={6}>
-                                <TableItemText>+7 (324) - 23 - 234</TableItemText>
-                            </Col>
-                            <Col xs={6}>
-                                <Button type="primary">Открыть</Button>
-                            </Col>
-                        </Row>
-                    </TableItem>
-                    <TableItem>
-                        <Row align="center">
-                            <Col xs={6}>
-                                <TableItemText>bvgv-2342-23fv-svvf</TableItemText>
-                            </Col>
-                            <Col xs={6}>
-                                <TableItemText>Аркаша Петухов</TableItemText>
-                            </Col>
-                            <Col xs={6}>
-                                <TableItemText>+7 (324) - 23 - 234</TableItemText>
-                            </Col>
-                            <Col xs={6}>
-                                <Button type="primary">Открыть</Button>
-                            </Col>
-                        </Row>
-                    </TableItem>
-                    <TableItem>
-                        <Row align="center">
-                            <Col xs={6}>
-                                <TableItemText>bvgv-2342-23fv-svvf</TableItemText>
-                            </Col>
-                            <Col xs={6}>
-                                <TableItemText>Аркаша Петухов</TableItemText>
-                            </Col>
-                            <Col xs={6}>
-                                <TableItemText>+7 (324) - 23 - 234</TableItemText>
-                            </Col>
-                            <Col xs={6}>
-                                <Button type="primary">Открыть</Button>
-                            </Col>
-                        </Row>
-                    </TableItem>
-                </Table>
+                {width > 400 ? <DesktopUsers /> : <MobileUsers />}
                 <Pagination defaultCurrent={1} total={50} />
             </Page>
         </PageWrapper>
